2766a77a2637d69c7685157e014936223cc52df9
KSuzuki <98202109+kbelltree@users.noreply.github.com>
2022-12-01 16:35:49 -0800

    Update a js file with five round game

--- javascript.js
+++ javascript.js
@@ -22,48 +22,105 @@ function getComputerChoice() {
 // Create a function named playRound for single round play that takes two parameters: playerSelection and computerSelection. 
 function playRound (playerSelection, computerSelection){
     // Change playerSelection argument to all lower case to accommodate all string case types. 
-    let playerSelectionLowerCased = playerSelection.toLowerCase(); 
-    // If playerSelection and computerSelection are the same, return "It's a tie!"
-    if (playerSelectionLowerCased === computerSelection){
-        return "It's a Tie!";
+    // let playerSelectionLowerCased = playerSelection.toLowerCase(); 
+    // (1 round game) IF playerSelection and computerSelection are the same, return "It's a tie!"
+    // (5 round game)IF it's a tie, alert and return 0; 
+    if (playerSelection === computerSelection){
+        console.log(`It's a Tie! Both chose ${playerSelection}.`);
+        return 0; 
     } 
     // Create conditionals that declare a Winner of the round with a string for each playerSelection.   
-    if (playerSelectionLowerCased === "rock"){
-        // If playerSelection is Rock and computerSelection is Scissors, return "You win! Rock beats Scissors."
+    if (playerSelection === "rock"){
+        // (1 round game)If playerSelection is Rock and computerSelection is Scissors, return "You win! Rock beats Scissors."
+        // (5 round game)IF player wins, alert and return 1
         if (computerSelection === "scissors"){
-            return "You Win! Rock beats Scissors."; 
+            console.log(`You Win! Computer chose ${computerSelection}. Rock beats Scissors.`); 
+            return 1;
         } 
-        // If playerSelection is Rock and computerSelection is Paper, return "You lose! Paper beats Rock."
+        // (1 round game)If playerSelection is Rock and computerSelection is Paper, return "You lose! Paper beats Rock."
+        // (5 round game)IF player loses, alert and return 2
         if (computerSelection === "paper"){
-            return "You Lose! Paper beats Rock.";
+            console.log(`You Lose! Computer chose ${computerSelection}. Paper beats Rock.`);
+            return 2;
         }
     }
-    if (playerSelectionLowerCased === "paper"){
-        // If playerSelection is Paper and computerSelection is Rock, return "You win! Paper beats Rock."
+    if (playerSelection === "paper"){
+        // (1 round game)If playerSelection is Paper and computerSelection is Rock, return "You win! Paper beats Rock."
+        // (5 round game)IF player wins, alert and return 1
         if (computerSelection === "rock"){
-            return "You Win! Paper beats Rock.";
+            console.log(`You Win! Computer chose ${computerSelection}. Rock beats Scissors.`); 
+            return 1; 
         }
-        // If playerSelection is Paper and computerSelection is Scissors, return "You lose! Scissors beats Paper."
+        // (1 round game)If playerSelection is Paper and computerSelection is Scissors, return "You lose! Scissors beats Paper."
+        // (5 round game)IF player loses, alert and return 2
         if (computerSelection === "scissors") {
-            return "You Lose! Scissors beats Paper.";
+            console.log(`You Lose! Computer chose ${computerSelection}. Paper beats Rock.`);
+            return 2;
         }
     }
-    if (playerSelectionLowerCased === "scissors"){
-        // If playerSelection is Scissors and computerSelection is Paper, return "You win! Scissors beats Paper."
+    if (playerSelection === "scissors"){
+        // (1 round game)If playerSelection is Scissors and computerSelection is Paper, return "You win! Scissors beats Paper."
+        // (5 round game)IF player wins, alert and return 1
         if (computerSelection === "paper"){
-            return "You Win! Scissors beats Paper.";
+            console.log(`You Win! Computer chose ${computerSelection}. Rock beats Scissors.`); 
+            return 1;
+
         }
-        // If playerSelection is Scissors and computerSelection is Rock, return "You lose! Rock beats Scissors."
+        // (1 round game)If playerSelection is Scissors and computerSelection is Rock, return "You lose! Rock beats Scissors."
+        // (5 round game)IF player loses, alert and return 2
         if (computerSelection === "rock"){
-            return "You Lose! Rock beats Scissors.";
+            console.log(`You Lose! Computer chose ${computerSelection}. Paper beats Rock.`);
+            return 2;
         }
-    }
-    // Create an error case that returns a message in case of misspelling or wrong element entry. 
-    else {
-        return "Improper entry. Please try again."
+    } else {
+        return;
     }
 }
-// Test run
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+// Create a new function named game() that plays 5 rounds. 
+function game() {
+    // Create variables that store the sum of winning count.
+        let playerSumOfWin = 0;
+        let computerSumOfWin = 0;
+        let sumOfTie = 0;
+    // Loop playRound 5 times    
+        for (let i = 0; i < 5; i++){
+           // Create a variable that store Player's entry in LOWER case. 
+           let promptPlayer = prompt("Rock, Paper, or Scissors?").toLowerCase(); 
+           // Call playRound function. 
+           let result = playRound(promptPlayer, getComputerChoice())
+           console.log(result);
+            //IF the condition returns TRUE, add one count to the appropriate sum variable. 
+            if (result === 0){
+                sumOfTie += 1; 
+            }
+            if (result === 1){
+                playerSumOfWin += 1; 
+            }
+            if (result === 2){
+                computerSumOfWin += 1;    
+            } 
+        }
+        let resultMessage = ``;
+    // IF the winning counts of player and computer are the same, return "It's a Tie!"
+    if (playerSumOfWin === computerSumOfWin){
+        resultMessage += `It's a Tie! You scored ${playerSumOfWin} and Computer scored ${computerSumOfWin}, Tied ${sumOfTie} times.`;
+    }
+    // IF the player's winning count is greater than the computer's, return "You Win!"
+    else if (playerSumOfWin > computerSumOfWin){
+        resultMessage += `You Win! You scored ${playerSumOfWin} and Computer scored ${computerSumOfWin}, Tied ${sumOfTie} times.`;
+    // Else return "You Lose!"
+    } else {
+        resultMessage += `You Lose! You scored ${playerSumOfWin} and Computer scored ${computerSumOfWin}, Tied ${sumOfTie} times.`;
+    }
+        console.log(resultMessage);
+        return resultMessage;
+    }
+    
+
+game();
+
+ 
+// Test run for single round
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
